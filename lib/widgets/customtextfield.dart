3db24be7cd1a71dd11929625be0f8544224e5080
abdi terefe <abdetaterefe@gmail.com>
2022-04-02 14:53:20 +0300

    made textfield validable

--- lib/widgets/customtextfield.dart
+++ lib/widgets/customtextfield.dart
@@ -5,12 +5,14 @@ class CustomTextField extends StatelessWidget {
   final TextInputAction? action;
   final bool show;
   final TextEditingController cust;
+  final String? Function(String?)? valid;
   const CustomTextField({
     Key? key,
     required this.hint,
     this.action,
     required this.show,
     required this.cust,
+    this.valid,
   }) : super(key: key);
 
   @override
@@ -23,6 +25,7 @@ class CustomTextField extends StatelessWidget {
         border: const OutlineInputBorder(),
         hintText: hint,
       ),
+      validator: valid,
     );
   }
 }
